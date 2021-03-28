f106d60c0ffb80d119a94b1d90e597a090bc6309
AmazingBeerBelly <amazingbeerbelly@gmail.com>
2021-03-28 15:50:50 +0800

    remove the annotation

--- test/method/createProject.test.js
+++ test/method/createProject.test.js
@@ -26,88 +26,88 @@ describe('Method Test - create_project', async () => {
     assert.strictEqual(_.isMatch(projectInfo, params), true);
   });
 
-  // it('Success case with value has some specific symbols', async () => {
-  //   const params = {
-  //     name: '\\_?*&^%$#@~!@name',
-  //     logo: '\\_?*&^%$#@~!https://oak.tech/_next/static/images/logo-e546db00eb163fae7f0c56424c3a2586.png',
-  //     description: '\\_?*&^%$#@~!description',
-  //     website: '\\_?*&^%$#@~!https://oak.tech/',
-  //   };
-  //   let error = null;
-  //   const projectInfo = await createProject(params).catch((err) => {
-  //     error = err.message;
-  //   });
-  //   assert.strictEqual(error, null);
-  //   assert.strictEqual(_.isMatch(projectInfo, params), true);
-  // });
+  it('Success case with value has some specific symbols', async () => {
+    const params = {
+      name: '\\_?*&^%$#@~!@name',
+      logo: '\\_?*&^%$#@~!https://oak.tech/_next/static/images/logo-e546db00eb163fae7f0c56424c3a2586.png',
+      description: '\\_?*&^%$#@~!description',
+      website: '\\_?*&^%$#@~!https://oak.tech/',
+    };
+    let error = null;
+    const projectInfo = await createProject(params).catch((err) => {
+      error = err.message;
+    });
+    assert.strictEqual(error, null);
+    assert.strictEqual(_.isMatch(projectInfo, params), true);
+  });
 
-  // it('Error case with value type is number', async () => {
-  //   const params = {
-  //     name: 123,
-  //     logo: 123,
-  //     description: 123,
-  //     website: 123,
-  //   };
-  //   let error = null;
-  //   await createProject(params).catch((err) => {
-  //     error = err.message;
-  //   });
-  //   assert.notEqual(error, null);
-  // });
+  it('Error case with value type is number', async () => {
+    const params = {
+      name: 123,
+      logo: 123,
+      description: 123,
+      website: 123,
+    };
+    let error = null;
+    await createProject(params).catch((err) => {
+      error = err.message;
+    });
+    assert.notEqual(error, null);
+  });
 
-  // it('Error case with value is null', async () => {
-  //   const params = {
-  //     name: null,
-  //     logo: null,
-  //     description: null,
-  //     website: null,
-  //   };
-  //   let error = null;
-  //   await createProject(params).catch((err) => {
-  //     error = err.message;
-  //   });
-  //   assert.notEqual(error, null);
-  // });
+  it('Error case with value is null', async () => {
+    const params = {
+      name: null,
+      logo: null,
+      description: null,
+      website: null,
+    };
+    let error = null;
+    await createProject(params).catch((err) => {
+      error = err.message;
+    });
+    assert.notEqual(error, null);
+  });
 
-  // it('Error case with value is empty string', async () => {
-  //   const params = {
-  //     name: '',
-  //     logo: '',
-  //     description: '',
-  //     website: '',
-  //   };
-  //   let error = null;
-  //   await createProject(params).catch((err) => {
-  //     error = err.message;
-  //   });
-  //   assert.notEqual(error, null);
-  // });
+  it('Error case with value is empty string', async () => {
+    const params = {
+      name: '',
+      logo: '',
+      description: '',
+      website: '',
+    };
+    let error = null;
+    await createProject(params).catch((err) => {
+      error = err.message;
+    });
+    assert.notEqual(error, null);
+  });
 
-  // it('Error case with value is empty array', async () => {
-  //   const params = {
-  //     name: [],
-  //     logo: [],
-  //     description: [],
-  //     website: [],
-  //   };
-  //   let error = null;
-  //   await createProject(params).catch((err) => {
-  //     error = err.message;
-  //   });
-  //   assert.notEqual(error, null);
-  // });
+  it('Error case with value is empty array', async () => {
+    const params = {
+      name: [],
+      logo: [],
+      description: [],
+      website: [],
+    };
+    let error = null;
+    await createProject(params).catch((err) => {
+      error = err.message;
+    });
+    assert.notEqual(error, null);
+  });
 
-  // it('Error case with value is empty object', async () => {
-  //   const params = {
-  //     name: {},
-  //     logo: {},
-  //     description: {},
-  //     website: {},
-  //   };
-  //   let error = null;
-  //   await createProject(params).catch((err) => {
-  //     error = err.message;
-  //   });
-  //   assert.notEqual(error, null);
-  // });
+  it('Error case with value is empty object', async () => {
+    const params = {
+      name: {},
+      logo: {},
+      description: {},
+      website: {},
+    };
+    let error = null;
+    await createProject(params).catch((err) => {
+      error = err.message;
+    });
+    assert.notEqual(error, null);
+  });
 });
